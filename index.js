90a75129b460df85cc30eb970e984674273f35d0
Rene Bridgman <renebridgman@Renes-iMac.local>
2023-04-13 19:39:52 +0200

    final commit

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+
+const slugify = require('slugify');
+
 const replaceTemplate = require('./modules/replaceTemplate');
 
 // SERVER
@@ -13,6 +16,11 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+// just learning about intergrating third party modules
+// const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+// console.log(slugs);
+// console.log(slugify('Fresh Avacado', { lower: true }));
 
 
 const server = http.createServer((req, res) => {
