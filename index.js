5dc35204fa59a68b49b0fe5a0946c5569797fcb0
Rene Bridgman <renebridgman@Renes-iMac.local>
2023-04-13 18:38:33 +0200

    make prettier

--- index.js
+++ index.js
@@ -4,42 +4,7 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 const replaceTemplate = require('./modules/replaceTemplate');
-/////////////////////////////////
-// FILES
 
-
-// blocking syncronous way
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-
-// const textOut = `Hey bitch! What yo saying my G? - ${textIn} \nCreated on ${Date.now()}`;
-
-// fs.writeFileSync('./txt/output.txt', textOut)
-
-// console.log('File has been written');
-
-
-// non-blocking async way
-
-// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-//     if(err) return console.log(err.message);
-//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-//         // console.log(data2);
-//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-//             // console.log(data3);
-
-//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, (err) => {
-//                 console.log('Your file has been written!')
-//             })
-//         });
-//     });
-// });
-
-// console.log("Will read file");
-
-
-
-
-///////////////////////////////////
 // SERVER
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
